e543aa5e69f2c0f23141f4fb590a5a3b7f418947
Samy Mahmoud <smh32001@gmail.com>
2023-09-22 00:51:23 +0300

    Header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 
+import SubHeading from '../../components/SubHeading/SubHeading';
+import images  from '../../constants/images';
 import './Header.css';
 
-const Header = () => (
-  <div>
-    Header
-  </div>
-);
+const Header = () => {
+  return (
+    <div className='app__header app__wrapper section__padding' id='home'>
+      <div className='app__wrapper_info'>
+        <SubHeading title='Chase The New Flavour'/>
+        <h1 className='app__header-h1'>The Key To Fine Dining</h1>
+        <p className='p__opensans' style={ { margin: '2rem 0' } } >
+          Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus 
+        </p>
+        <button type='button' className='custom__button'>Explore Menu</button>
+      </div>
+      <div className='app__wrapper_img'>
+        <img src={images.welcome} alt='header img' />
+      </div>
+    </div>
+  )
+}
 
 export default Header;
