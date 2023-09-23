4c2d52331337e649d0d6c4dea14be6d62e94b8dc
Samy Mahmoud <smh32001@gmail.com>
2023-09-23 14:48:32 +0300

    Chef

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
-
+import { SubHeading } from '../../components';
+import images  from '../../constants/images';
 import './Chef.css';
 
-const Chef = () => (
-  <div>
-    Chef
-  </div>
-);
+const Chef = () => {
+  return (
+    <div className='app__bg app__wrapper section__padding'>
+      <div className='app__wrapper_img app__wrapper_img-reverse'>
+        <img src={ images.chef } alt='chef' />
+      </div>
+      <div className='app__wrapper_info'>
+        <SubHeading title="Chef`s Word" />
+        <h1 className='headtext__cormorant'>What We believe in</h1>
+
+        <div className='app__chef-content'>
+          <div className='app__chef-content_quote'>
+            <img src={ images.quote } alt='quote' />
+            <p className='p__opensans'></p>
+          </div>
+          <p className='p__opensans'></p>
+        </div>
+
+        <div className='app__chef-sign'>
+          <p>Kevin Luo</p>
+          <p className='p__opensans'>Chef & Founder</p>
+          <img src={ images.sign } alt='sign' />
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default Chef;
