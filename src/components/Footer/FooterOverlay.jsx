462b36ebe6e56830aa9c92ea68e263a946a1db18
Samy Mahmoud <smh32001@gmail.com>
2023-09-26 00:48:51 +0300

    Footer

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 
 import './FooterOverlay.css';
 
-const FooterOverlay = () => (
-  <div>
-    FooterOverlay
-  </div>
-);
+const FooterOverlay = () => {
+  return (
+    <div className='app__footerOverlay'>
+      <div className='app__footerOverlay-black'></div>
+      <div className='app__footerOverlay-img app__bg'>
+        
+      </div>
+    </div>
+  )
+}
 
 export default FooterOverlay;
--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -16,9 +16,11 @@ const Chef = () => {
         <div className='app__chef-content'>
           <div className='app__chef-content_quote'>
             <img src={ images.quote } alt='quote' />
-            <p className='p__opensans'></p>
+            <p className='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit auctor sit .</p>
           </div>
-          <p className='p__opensans'></p>
+          <p className='p__opensans'>
+            auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit. Nulla scelerisque scelerisque congue ac consequat, aliquam molestie lectus eu. Congue iaculis integer curabitur semper sit nunc.
+          </p>
         </div>
 
         <div className='app__chef-sign'>
